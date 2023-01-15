c0668c9780289a883872f8fd2c4f02f459e8888e
Noah Everett <noaheverett898@gmail.com>
2023-01-14 17:05:33 -0700

    Changes

---
+++ README.md
@@ -0,0 +1,27 @@
+# ANNIEDirt - Docker branch
+
+## About
+ANNIEDirt (g4annie_dirt_flux, g4dirt, g4annie, etc.) is part of ANNIE's simulation chain. It propigates final state particles from [GENIE](https://github.com/ANNIEsoft/GENIE-v3) until they are inside ANNIE's tank volume (or anyother specified volumes). This can drastically reduce run time when studing neutrino events outside the ANNIE tank.
+
+## `g4annie_dirt_flux` Usage
+```
+g4annie_dirt_flux -h | --help           print usage statment
+                  -b | --batch          run in batch mode
+                  -v | --verbose        increase verbosity
+                  -n | --nevents        limit # of events
+                  -r | --repeat         repeat each GENIE event in G4
+                  -g | --gdml=GFILE     input geometry file
+                  -p | --physics=PNAME  PhysicsList name
+                  -i | --input=GNTP     GENIE file
+                  -o | --output=OFILE   output file
+```
+
+## `run_g4dirt.sh` Usage
+```
+run_g4dirt.sh -r=<run number (or numbers using `*`. Ex: \`-r='4*'\`)>
+              -i=</path/to/input/GENIE/files/dir>
+              -n=<number of events per GENIE file to propigate>
+              -g=</path/to/geometry/file.gdml>
+              -o=</path/to/output/dir>
+              -h|--help
+```
---
+++ run_g4dirt.sh
@@ -0,0 +1,91 @@
+#! /usr/bin/env bash
+
+main() {
+export INDIR=""
+export OUTDIR=""
+export VERBOSE=0
+export RWHNODE=0
+export RUNNUM=""  # starting run number
+export BATCH=""
+
+for i in "$@"; do
+  case $i in
+    -r=*        ) export RUNNUM="${i#*=}"               shift    ;;
+    -i=*        ) export INDIR="${i#*=}"                shift    ;;
+    -n=*        ) export NEVENTS="${i#*=}"              shift    ;;
+    -g=*        ) export GEOMETRY="${i#*=}"             shift    ;;
+    -o=*        ) export OUTDIR="${i#*=}"               shift    ;;
+    -h*|--help* ) usage;                                return 1 ;;
+    -*          ) echo "Unknown option \`$i\`."; usage; return 1 ;;
+  esac
+done
+
+if [ -z "${RUNNUM}" ]; then
+  echo "Use \`-r=\` to set the run number (or numbers using \`*\`. Ex: \`-r='4*'\`)."
+  return 1
+fi
+
+if [ -z "${INDIR}" ]; then
+  echo "Use \`-i=\` to set the input directory (ex: -p=/pnfs/annie/persistent/users/...)."
+  return 2
+fi
+
+if [ -z "${NEVENTS}" ]; then
+  echo "Use \`-n=\` to set the number of events per input file to propigate (-n=-1 for all)."
+  return 3
+fi
+
+if [ -z "${GEOMETRY}" ]; then
+  echo "Use \`-g=\` to set the annie geometry (ex: -g=/pnfs/annie/persistent/users/.../name.gdml)"
+  return 4
+fi
+
+if [ -z "${OUTDIR}" ]; then
+  echo "Use \`-o=\` to set the output directory (will create a new file if needed)."
+  return 5
+fi
+
+export USEPHYLIST="FTFP_BERT_HP"
+mkdir ${OUTDIR}
+cd ${INDIR}
+
+for INFILE in ${INDIR}/gntp.${RUNNUM}.ghep.root; do 
+  if [ -f "${INFILE}" ]; then
+    export CURRUNNUM=$(basename ${INFILE})
+    export CURRUNNUM=${CURRUNNUM#gntp.}
+    export CURRUNNUM=${CURRUNNUM%.ghep.root}
+    export OUTFILE=annie_tank_flux.${CURRUNNUM}.root
+    export OUTFILELOG=annie_tank_flux.${CURRUNNUM}.log
+
+cat <<EOF > ${OUTFILELOG}
+#=============== RUN SETTINGS ===============#
+  GENIE file (in dir): ${INFILE}
+g4dirt file (out dir): ${OUTFILE}
+      g4dirt file log: annie_tank_flux.${CURRUNNUM}.log
+             commmand: $B/g4annie_dirt_flux --batch -n ${NEVENTS} -g ${GEOMETRY} --physics=${USEPHYLIST} -i ${INFILE} -o $(basename ${OUTFILE})
+EOF
+
+# I know the `-i ./$basename ${INFILE})` looks really dumb, but its necessary... I learned the hard way
+/ANNIEDirt_install/bin/g4annie_dirt_flux --batch -n ${NEVENTS} -g ${GEOMETRY} --physics=${USEPHYLIST} -i ./$(basename ${INFILE}) -o $(basename ${OUTFILE}) 2>&1 | tee -a ${OUTFILELOG}
+
+mv annie_tank_flux.* ${OUTDIR}
+rm currentEvent.rndm
+rm currentRun.rndm
+  fi
+done
+
+cd -
+}
+
+usage() {
+cat >&2 <<EOF
+run_g4dirt.sh -r=<run number (or numbers using `*`. Ex: \`-r='4*'\`)>
+              -i=</path/to/input/GENIE/files/dir>
+              -n=<number of events per GENIE file to propigate>
+              -g=</path/to/geometry/file.gdml>
+              -o=</path/to/output/dir>
+              -h|--help
+EOF
+}
+
+main "$@"
